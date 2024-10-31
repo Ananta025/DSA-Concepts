2360c2a96ac0a962817b22205a8a9365f510e0fd
Ananta022 <danantach@gmail.com>
2024-10-31 13:03:22 +0530

    commit

---
+++ Graphs - 2/chippestFlight.java
@@ -0,0 +1,13 @@
+/*
+    *Cheapest Flights within K Stops
+        There are n cities connected by some number of flights. You are given an array flights where
+        flights[il = [from, to, pricel indicates that there is a flight.
+        You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops.
+        If there is no such route, return -1.
+ */
+
+public class chippestFlight {
+    public static void main (String args[]){
+
+    }
+}
