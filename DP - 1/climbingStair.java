69ffbb1a3b7e4399b2caaf61fcbb6acd1aaa901d
Ananta022 <danantach@gmail.com>
2024-11-01 21:54:14 +0530

    commit

---
+++ DP - 1/climbingStair.java
@@ -0,0 +1,9 @@
+/*
+    *Climbing Stairs
+        Count ways to reach the nth stair. The person can climb either 1 stair or 2 stairs at a time.
+*/
+public class climbingStair {
+    public static void main (String args[]){
+
+    }
+}
